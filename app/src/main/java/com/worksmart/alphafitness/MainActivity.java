1a0ca4405708cf4df63e90fe6cee0d3c956e3e4a
Alexander Kamensky <alexander.kamensky@sjsu.edu>
2018-11-22 14:33:52 -0800

    started work on database, some updates to data model
    - DBHelper Class
    - UserProfile table
    - Workout table
    - method to set and get data
    - for now presistance is only in the main application work to be done with regards to service
    
    Signed-off-by: Alexander Kamensky <alexander.kamensky@sjsu.edu>

--- app/src/main/java/com/worksmart/alphafitness/MainActivity.java
+++ app/src/main/java/com/worksmart/alphafitness/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Main", "Calling on create");
-        ModelTest.testModel();
+        DBHelper database = new DBHelper(this);
+        ModelTest.testModel(database);
     }
 }
--- app/src/main/java/com/worksmart/alphafitness/Workout.java
+++ app/src/main/java/com/worksmart/alphafitness/Workout.java
@@ -1,14 +1,27 @@
 package com.worksmart.alphafitness;
 
 public class Workout {
+    private Integer id;
     private Integer totalCalories;
     private Integer time;
     private double distance;
     private Integer week;
 
+    public static Integer idCount=0;
+
 
     public Workout(Integer week){
+        this.id = idCount;
+        idCount = idCount +1;
         this.week  = week;
+    }
+
+    public Workout(Integer id, Integer week, Integer totalCalories, Integer time, double distance){
+        this.id = id;
+        this.week = week;
+        this.totalCalories = totalCalories;
+        this.time = time;
+        this.distance = distance;
     }
 
     public void setTotalCalories(Integer totalCalories) {
@@ -37,5 +50,9 @@ public class Workout {
 
     public Integer getWeek() {
         return week;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
