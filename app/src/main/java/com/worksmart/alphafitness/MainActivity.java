5622bccbcf346a83eaf831db7245e82dca164c24
sjsuser <david.barban@sjsu.edy>
2018-11-23 17:12:20 -0800

    add launch map button

--- app/src/main/java/com/worksmart/alphafitness/MainActivity.java
+++ app/src/main/java/com/worksmart/alphafitness/MainActivity.java
@@ -1,8 +1,11 @@
 package com.worksmart.alphafitness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d("Main", "Calling on create");
         DBHelper database = new DBHelper(this);
         ModelTest.testModel(database);
+
+        Button launchMapButton = (Button)findViewById(R.id.launchMapButton);
+
+        launchMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
